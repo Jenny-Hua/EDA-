1852a9a1b6cfd24f410d8e4cf5522b1c9d98ccff
Jenny-Hua <jhuancaan@unsa.edu.pe>
2022-10-07 08:58:05 -0500

    First implementation of RBTree program

--- LAB02-RBTree/main.cpp
+++ LAB02-RBTree/main.cpp
@@ -6,12 +6,49 @@ similar al que se muestra en Fig. 2
 **/
 
 #include <iostream>
+#include "rwfile.h"
+#include "RBTree.h"
 using namespace std;
 
 
+ 
+// Constantes para RWFILE
+const int MAXNUM = 100000; 
+const int TAM = 100000;
+std::string NUMFILE = "numbers.txt";
+std::string TIMEFILE = "times.csv";
 
-int main()
-{
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    //0. Generacion de archivo 'numbers.txt' con 100 000 datos
+    /* srand(time(NULL));
+    generadorNumRandom(NUMFILE, TAM, MAXNUM); */
+
+    //1. Creación de un RBTree
+    RBTree mytree;
+
+    //2. Leer el archivo NUMFILE para agregar cada file Number a RBTree
+    int num, counter=0;
+    ifstream numbers;
+    numbers.open(NUMFILE);
+    while (numbers>>num)
+    {
+        // Insertar num a mytree
+        cout<<num<<"\n";
+        mytree.insert(num);
+        counter++;
+    }
     
-    return 0;
+    //3. Guardar el tiempo de acceso (búsqueda)
+    //      con datos desde 10 a 10 000 y mida el tiempo medio de accesos 
+    //      partiendo desde la ra ́ız hasta un nodo aleatorio.
+    
+    
+    //4. Impresión de arbol
+	mytree.prettyPrint();
+    cout<<"\nReadFileCounter: "<<counter<<"\n\n";
+	return 0;
 }
---
+++ LAB02-RBTree/rwfile.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <chrono>
+
+
+// Crear un archivo con una lista de TAM números de 0 a MAXNUM
+void generadorNumRandom(std::string nameFile, int TAM, int MAXNUM)
+{
+    std::ofstream numbersFile;
+    numbersFile.open(nameFile);
+
+    int num;
+    for (int i = 0; i < TAM; i++)
+    {
+        num = 1 + rand() % (MAXNUM);
+        numbersFile << num << "\n";
+    }
+    numbersFile.close();
+}
+
+
+// Guardar en archivo TIMEFILE la DURATION de ejecución en milisec de un INPUT (entrada)
+void timesFile(std::string TIMEFILE ,int input, std::chrono::milliseconds duration)
+{
+    // Crear archivo
+    std::ofstream outfile;
+    outfile.open(TIMEFILE, std::ios::app);
+    int _duration = duration.count();
+    outfile << input << ", " << _duration << "\n";
+    outfile.close();
+}
