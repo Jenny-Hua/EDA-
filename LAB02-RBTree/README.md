72530e083dda5d2472a0163c79911a7c65f69e3e
Jenny-Hua <jhuancaan@unsa.edu.pe>
2022-10-07 07:52:55 -0500

    Add Readme

---
+++ LAB02-RBTree/README.md
@@ -0,0 +1,6 @@
+# Arbol Negro Rojo
+
+Recursos revisados:
+
+1. https://algorithmtutor.com/Data-Structures/Tree/Red-Black-Trees/
+2. 
---
+++ LAB02-RBTree/main.cpp
@@ -0,0 +1,17 @@
+/** 
+ * Implemente el  ́arbol Red-Black. Ejecute el algoritmo varias veces con datos desde 10 a 10 000 y mida el
+tiempo medio de accesos partiendo desde la ra ́ız hasta un nodo aleatorio. Tendr ́a que obtener un gr ́afico
+similar al que se muestra en Fig. 2
+* 2. Analizar la complejidad computacional
+**/
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    
+    return 0;
+}
